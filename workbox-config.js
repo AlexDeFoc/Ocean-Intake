198c87b1fecb6e200c0577b7f25029f11d7760e7
AlexDeFoc <alex.de.foc@gmail.com>
2024-05-20 01:12:54 +0300

    ....idk

--- workbox-config.js
+++ workbox-config.js
@@ -1,29 +1,9 @@
-// module.exports = {
-// 	globDirectory: './',
-// 	globPatterns: [
-// 		'**/*.{ico,png,jpg,html,json,js,css}'
-// 	],
-// 	swDest: 'sw.js',
-// 	ignoreURLParametersMatching: [
-// 		/^utm_/,
-// 		/^fbclid$/
-// 	]
-// };
-
-// import {registerRoute} from 'workbox-routing';
-// import {NetworkFirst} from 'workbox-strategies';
-
-// registerRoute(
-//   ({url}) => url.pathname.startsWith('./index.html'),
-//   new NetworkFirst()
-// );
-
 module.exports = {
     globDirectory: './',
     globPatterns: [
         '**/*.{ico,png,jpg,html,json,js,css}'
     ],
-    swDest: 'sw.js',
+    swDest: './sw.js',
     ignoreURLParametersMatching: [
         /^utm_/,
         /^fbclid$/
