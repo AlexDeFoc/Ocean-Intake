7c724832f60b64f0e640f7122128391d1a08bdd9
AlexDeFoc <alex.de.foc@gmail.com>
2024-05-23 08:16:50 +0300

    leave just one sw

--- script.js
+++ script.js
@@ -115,12 +115,3 @@ theme_icon.addEventListener("click", () => {
         console.error('No themes available');
     }
 });
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-    .then(function(registration) {
-        console.log('Service Worker registered with scope:', registration.scope);
-    }).catch(function(error) {
-        console.log('Service Worker registration failed:', error);
-    });
-}
